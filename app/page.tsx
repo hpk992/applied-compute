ed6303a954e36c43c5cd20d3895aad62519af9d1
hpk992 <83901776+hpk992@users.noreply.github.com>
2025-10-22 13:01:41 -0700

    added fonts

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Button from '@/app/components/ui/Button';
 export default function Home() {
   return (
     <main>
-      <p className="font-normal">
+      <p className="font-suisse-works">
         Like human top performers, agent top performers must be intelligent, dedicated, and the best
         in the world at what they do. And like elite employees, they are non-fungible; they must be
         highly specialized for your industry, your company, and your work.
